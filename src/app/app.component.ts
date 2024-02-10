886bac1f2c2814875fc222270b64248413314a2f
Dheeraj S <dheerajstvpm@gmail.com>
2024-02-10 23:54:01 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularForNode';
+  backendText :any;
+  private backendURL = "http://localhost:4000/api"
+
+  constructor(private http: HttpClient) { }
+
+  signupUser() {
+    return this.http.get(this.backendURL)
+  }
+
+  ngOnInit() {
+    this.signupUser().subscribe((res) => {
+      console.log(res);
+
+      this.backendText = res;
+    })
+  }
 }
